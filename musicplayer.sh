3d84af236c12c97c657c727d7aad8570a2ba3bd8
Aaron Bocksberger <aaron-b1@gmx.de>
2025-10-01 20:09:34 +0200

    added selection of mutiple songs at once

--- musicplayer.sh
+++ musicplayer.sh
@@ -1,16 +1,17 @@
 #!/bin/dash
 
-music_dir="/path/to/music/directory"
+music_dir="/path/to/music/directory/"
 
 while true; do
-	select_song=$(find "$music_dir" -type f | sed "s|$music_dir/||" | fzf --reverse)
+	select_song=$(find "$music_dir" -type f | sed "s|$music_dir||" | fzf --multi --reverse)
 	[ -z "$select_song" ] && break
+    song_path=$(echo "$select_song" | sed "s|^|$music_dir|" | sed "s|^|\"|" | sed "s|$|\"|")
 	clear
 
 	if [ "$1" = "a" ]; then
-		mpv --no-video "$music_dir/$select_song"
+		echo $song_path | xargs mpv --no-video
 	else
-		mpv "$music_dir/$select_song"
+		echo $song_path | xargs mpv
 	fi
 done
 
